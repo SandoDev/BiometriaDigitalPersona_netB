ed29374f5d971a6e9f0fc0ec0fc5a505a0b0ebca
sandodev-w10 <camilo.sandoval.ad@gmail.com>
2022-03-14 21:42:10 -0500

    Fix localDateTime with TimeZone New env vars

--- src/DAO/AssistanceDAO.java
+++ src/DAO/AssistanceDAO.java
@@ -5,20 +5,22 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.TimeZone;
 import model.Database;
+import config.PropertiesFile;
 
 public class AssistanceDAO {
-    
+
     Database con = new Database();
-        
-    public void registerAssistance(Integer inscription) throws SQLException{
+    private String timeZone = "America/Bogota";
+
+    public void registerAssistance(Integer inscription) throws SQLException {
         String query = "insert into Assistance "
                 + "(`entryTime`, `inscription_idInscription`) "
                 + "values(?, ?)";
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         Connection c = con.getConnection();
         PreparedStatement stm = c.prepareStatement(query);
-        stm.setString(1, dtf.format(LocalDateTime.now()));
+        stm.setString(1, this.getLocalDate());
         stm.setInt(2, inscription);
         stm.execute();
         stm.close();
@@ -26,4 +28,17 @@ public class AssistanceDAO {
         con.disconnect();
     }
 
+    /**
+     * Obtiene la fecha local con el formato en string adecuado y basado en el
+     * tiempo zonal
+     *
+     * @return localDateTime -> "2022-03-03 10:00:00"
+     */
+    private String getLocalDate() {
+        PropertiesFile properties = new PropertiesFile("database");
+        this.timeZone = properties.getProperty("timeZone");
+        TimeZone zone = TimeZone.getTimeZone(this.timeZone);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return dtf.format(LocalDateTime.now(zone.toZoneId()));
+    }
 }
